f1ec3db471f83ca537a1a228ae19850fe5b79e90
Darjetii-Dooku <7220339@gmail.com>
2023-11-03 14:26:04 +0200

    base

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var API_KEY = '40440895-32c85d5bb5a63add0d6a3e6ca';
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});
